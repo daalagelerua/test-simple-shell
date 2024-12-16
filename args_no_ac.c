03e75bc7f4ff76bcd5fbecdd9bd985f4f99c1862
daalagelerua <aurele284@gmail.com>
2024-12-16 10:14:08 +0000

    ajout de commentaire args_no_ac

--- args_no_ac.c
+++ args_no_ac.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+* av est un double pointeur car:
+* chaque argument est une chaine de caractere, ces chaines sont stockees dans un tableau de pointeurs
+* chaque element du tableau est un pointeur vers un char (une chaine)
+* si on utilisait un simple pointeur la ligne de commande serait vu commme une seule chaine de caractere
+*/
+
 int main(int argc, char **av)
 {
     (void)argc; // On ignore argc pour ne pas recevoir de warning
