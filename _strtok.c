2217e55b7adcde893a48d2b67792ed96a51716c5
daalagelerua <aurele284@gmail.com>
2024-12-16 12:28:28 +0000

    fonction _strtok ajout de comments

--- _strtok.c
+++ _strtok.c
@@ -4,7 +4,8 @@
 /**
 * la fonction strtok sert a divisee une chaine en plusieurs sous-chaine appelees token,
 * en fonction d'un ou plusieurs delimiteurs (ex: chaque " ")
-* une ligne CSV (comma-separated values) serait une  chaine separee par des virgules
+* une ligne CSV (comma-separated values) serait une  chaine separee par
+* des virgules ou autres delimiteurs (, ; tab espace |)
 */
 int main(void) {
     char str[] = "Hello world this is C programming";
